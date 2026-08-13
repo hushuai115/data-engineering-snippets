b06792a7d989f1fa5b7daca889b888449eab97a9
hushuai115 <hushuai115@gmail.com>
2026-08-13 22:23:57 +0800

    1

--- 01-sql-and-data-warehousing/day03_conditional_aggregation_and_nulls.sql
+++ 01-sql-and-data-warehousing/day03_conditional_aggregation_and_nulls.sql
@@ -1,8 +1,13 @@
-SELECT t.user_id,t.dep_id
+SELECT
+    t.user_id
     , count(*) AS total_orders
     , count(*) FILTER (WHERE t.order_status = 'COMPLETED') AS completed_orders
     , coalesce(sum(t.order_amount) FILTER (WHERE t.order_status = 'COMPLETED') , 0) AS total_completed_amount
     , round((count(1) FILTER (WHERE t.order_status = 'CANCELLED')) * 1.0 / count(*) , 2) AS cancellation_rate
-FROM user_orders t
-GROUP BY t.user_id,t.dep_id
-ORDER BY t.user_id,t.dep_id;
+FROM
+    user_orders t
+GROUP BY
+    t.user_id
+ORDER BY
+    t.user_id;
+
